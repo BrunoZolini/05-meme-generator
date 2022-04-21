f08dd79337255b715e0cf612512c3cd261a7847e
Bruno Zolini <bruno.zolini@hotmail.com>
2022-04-21 10:05:02 -0300

    Requisito 2 e 3

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const inputText = document.getElementById('text-input');
-const memeText = document.getElementById('meme-text');
+const inputText = document.querySelector('#text-input');
+const inputFile = document.querySelector('#meme-insert');
+// const memeContainer = document.querySelector('#meme-image-container');
+const memeText = document.querySelector('#meme-text');
+const memeImage = document.querySelector('#meme-image');
 
 inputText.addEventListener('keyup', () => {
   const texto = inputText.value;
   memeText.innerText = texto;
+});
+
+inputFile.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  memeImage.src = URL.createObjectURL(file);
+  // memeContainer.appendChild(memeImage);
 });
