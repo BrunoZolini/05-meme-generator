66644378d3ae5e6c88b3309d75eeba49c7a72e7d
Bruno Zolini <bruno.zolini@hotmail.com>
2022-04-21 10:42:13 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const inputText = document.querySelector('#text-input');
 const inputFile = document.querySelector('#meme-insert');
-// const memeContainer = document.querySelector('#meme-image-container');
+const memeContainer = document.querySelector('#meme-image-container');
 const memeText = document.querySelector('#meme-text');
 const memeImage = document.querySelector('#meme-image');
+const elementsButtons = document.querySelector('#elements-buttons');
 
 inputText.addEventListener('keyup', () => {
   const texto = inputText.value;
@@ -13,4 +14,11 @@ inputFile.addEventListener('change', (event) => {
   const file = event.target.files[0];
   memeImage.src = URL.createObjectURL(file);
   // memeContainer.appendChild(memeImage);
+});
+
+elementsButtons.addEventListener('click', (event) => {
+  const buttonSelected = event.target.id;
+  if (buttonSelected === 'fire') memeContainer.style.border = '3px dashed red';
+  if (buttonSelected === 'water') memeContainer.style.border = '5px double blue';
+  if (buttonSelected === 'earth') memeContainer.style.border = '6px groove green';
 });
